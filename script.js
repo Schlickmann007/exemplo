66dc683051bfc9afe0a92fa688288e87311fc0f8
Edilson <schlickmann1@hotmail.com>
2021-04-01 20:03:10 -0300

    adicionada a validaçao de nome vazio no formulario

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function helloworld() {
     var inputs = document.getElementsByTagName('input');
     var input = inputs[0];
-    var nome = input.value;
+    var nome = input.value.trim();
+
+    if (nome === '') {
+        alert('Informe o seu nome!');
+        return;
+
+
+
+    }
     console.log('nome: ', nome);
     window.alert('Bem vindo ! ' + nome);
     input.value = '';
